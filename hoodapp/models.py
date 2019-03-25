c93209fbaad740a0a92db7630f9ca7ce2f5ca688
blackrose <wahomekelving@gmail.com>
2019-03-25 11:01:19 +0300

    Added update occupants Neighborhood method

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -3,38 +3,42 @@ import datetime as dt
 from django.contrib.auth.models import User
 
 
-
 class Neighbourhood(models.Model):
-      '''
-      class that contains Neighbourhood properties
-      '''
-      NEIGHBOURHOOD_CHOICES = (
-        ('South B','South B'),
-        ('South C','South C'),
-        ('Westlands','Westlands'),
-        ('Donholm','Donholm'),
-        ('Ruaka','Ruaka'),
-        ('Imara Daima','Imara Daima'),
-        ('Syokimau','Syokimau'),
-        ('Buruburu','Buruburu'),
-        ('Kinoo','Kinoo'),
-        ('Komarock','Komarock'),
-        ('Madaraka','Madaraka'),
-        ('Rongai','Rongai'),
-        ('Karen','Karen'),
-        ('Ruiru','Ruiru'),
-        ('Roysambu','Roysambu'),
-        ('Juja','Juja'),
-      )
-      name = models.CharField(max_length=200)
-      location = models.CharField(max_length=30,choices=NEIGHBOURHOOD_CHOICES)
-      description = models.TextField(blank=True,null=True)
-      posted_on = models.DateTimeField(auto_now_add=True)
-      police_dept = models.IntegerField(default="0722445233")
-      health_dept = models.IntegerField(default="0700505221")
-      user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    '''
+    class that contains Neighbourhood properties
+    '''
+    NEIGHBOURHOOD_CHOICES = (
+        ('South B', 'South B'),
+        ('South C', 'South C'),
+        ('Westlands', 'Westlands'),
+        ('Donholm', 'Donholm'),
+        ('Ruaka', 'Ruaka'),
+        ('Imara Daima', 'Imara Daima'),
+        ('Syokimau', 'Syokimau'),
+        ('Buruburu', 'Buruburu'),
+        ('Kinoo', 'Kinoo'),
+        ('Komarock', 'Komarock'),
+        ('Madaraka', 'Madaraka'),
+        ('Rongai', 'Rongai'),
+        ('Karen', 'Karen'),
+        ('Ruiru', 'Ruiru'),
+        ('Roysambu', 'Roysambu'),
+        ('Juja', 'Juja'),
+    )
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=30, choices=NEIGHBOURHOOD_CHOICES)
+    description = models.TextField(blank=True, null=True)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    police_dept = models.IntegerField(default="0722445233")
+    health_dept = models.IntegerField(default="0700505221")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
 
-      def save_neighbourhood(self):
+    def save_neighbourhood(self):
         self.save()
-      def update_neighbourhood(self):
+
+    def update_neighbourhood(self):
+        self.update()
+
+    def update_occupants(self):
         self.update()
