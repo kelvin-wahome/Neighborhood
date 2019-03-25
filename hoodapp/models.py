181aa3bcef2b502e0d3befa8804a5ac304b5bfe2
blackrose <wahomekelving@gmail.com>
2019-03-25 11:50:22 +0300

    Added delete comment method

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -188,3 +188,9 @@ class Comments(models.Model):
 
     def save_comment(self):
         self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    def __str__(self):
+        return self.comment
