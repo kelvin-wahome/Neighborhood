c38e6e45a9db42cb2f1cf4d3b64d66fd3112aeb9
blackrose <wahomekelving@gmail.com>
2019-03-25 09:50:13 +0300

    Added index view function and urls

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
-# Create your views here.
+from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+  '''
+  View function that renders the homepage
+  '''
+  neighbourhoods = Neighbourhood.objects.all()
+  return render(request,'index.html',locals())
