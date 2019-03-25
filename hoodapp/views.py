34f52749f81bcb0d4b3b10f4a077a6b1a90eba95
blackrose <wahomekelving@gmail.com>
2019-03-25 15:05:42 +0300

    Added search view function and template

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -13,3 +13,18 @@ def index(request):
   '''
   neighbourhoods = Neighbourhood.objects.all()
   return render(request,'index.html',locals())
+  
+@login_required(login_url='/accounts/login/')
+def search_business(request):
+
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        businesses = Business.objects.filter(name__icontains = search_term,hood = request.user.join.hood_id.id)
+        searched_businesses = Business.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',locals())
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',locals())
