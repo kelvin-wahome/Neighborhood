3d41826587c2f00749be3d91f39f0f21a25a8a4a
blackrose <wahomekelving@gmail.com>
2019-03-25 20:14:59 +0300

    Added join and add hood urls

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   url('^$',views.index,name = 'index'),
+   url(r'^add_hood/$', views.add_hood, name='add_hood'),
+  url(r'^join/(\d+)',views.join_hood,name='join_hood'),
   url(r'^profile/$',views.profile,name = 'profile'),
   url(r'^update_profile/$',views.update_profile,name= 'update_profile'),
 ]
