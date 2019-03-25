aee77436838ee681fc185690b442d7e713097a01
blackrose <wahomekelving@gmail.com>
2019-03-25 20:19:24 +0300

    Added home url

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -3,13 +3,16 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns=[
-  url('^$',views.index,name = 'index'),
-   url(r'^add_hood/$', views.add_hood, name='add_hood'),
-  url(r'^join/(\d+)',views.join_hood,name='join_hood'),
-  url(r'^profile/$',views.profile,name = 'profile'),
-  url(r'^update_profile/$',views.update_profile,name= 'update_profile'),
+urlpatterns = [
+    url('^$', views.index, name='index'),
+    url(r'^add_hood/$', views.add_hood, name='add_hood'),
+    url(r'^join/(\d+)', views.join_hood, name='join_hood'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^update_profile/$', views.update_profile, name='update_profile'),
+    url(r'^home/$', views.home, name='home'),
+
 ]
 
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
