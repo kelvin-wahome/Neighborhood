39c251147425bb42b086b8ac507ad1f722fca0f1
blackrose <wahomekelving@gmail.com>
2019-03-25 10:57:27 +0300

    Added Neighborhood model

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Neighbourhood(models.Model):
+  '''
+  class that contains Neighbourhood properties
+  '''
+  NEIGHBOURHOOD_CHOICES = (
+    ('South B','South B'),
+    ('South C','South C'),
+    ('Westlands','Westlands'),
+    ('Donholm','Donholm'),
+    ('Ruaka','Ruaka'),
+    ('Imara Daima','Imara Daima'),
+    ('Syokimau','Syokimau'),
+    ('Buruburu','Buruburu'),
+    ('Kinoo','Kinoo'),
+    ('Komarock','Komarock'),
+    ('Madaraka','Madaraka'),
+    ('Rongai','Rongai'),
+    ('Karen','Karen'),
+    ('Ruiru','Ruiru'),
+    ('Roysambu','Roysambu'),
+    ('Juja','Juja'),
+  )
+  name = models.CharField(max_length=200)
+  location = models.CharField(max_length=30,choices=NEIGHBOURHOOD_CHOICES)
+  description = models.TextField(blank=True,null=True)
+  posted_on = models.DateTimeField(auto_now_add=True)
+  police_dept = models.IntegerField(default="0722445233")
+  health_dept = models.IntegerField(default="0700505221")
+  user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    url('^$',views.index,name = 'index'),
+urlpatterns=[
+  url('^$',views.index,name = 'index'),
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
