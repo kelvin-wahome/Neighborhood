24bd0a2462010ed2d70a504aec5e45c9c897a765
blackrose <wahomekelving@gmail.com>
2019-03-27 07:25:49 +0300

    last commit

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Neighbourhood,Profile,Business,Posts,Join,Comments
 
-# Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Join)
+admin.site.register(Posts)
+admin.site.register(Comments)
